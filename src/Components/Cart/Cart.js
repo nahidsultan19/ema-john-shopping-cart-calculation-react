160e8c63d969154b5420482ab9f9ace36fd6736e
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-25 00:50:20 +0600

    ema-john simple shopping cart calculation using react

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const { cart } = props;
+
+    let total = 0;
+    let shipping = 0;
+
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = (total * 10 / 100).toFixed(2);
+    const result = total + shipping + parseFloat(tax);
+    return (
+        <div className='cart'>
+            <h2 className='order'>Order Summary</h2>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price:$ {total}</p>
+            <p>Total Shipping chanrge:$ {shipping}</p>
+            <p>Tax:$ {tax}</p>
+            <h4>Grand Total:$ {result}</h4>
+        </div>
+    );
+};
+
+export default Cart;
